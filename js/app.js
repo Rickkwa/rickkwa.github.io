af987d77cbd090b33fa18ec8dafa7394b79ee325
Ricky K <rickyk.162@gmail.com>
2016-06-18 01:47:41 -0400

    Added some more links and projects json template

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = app || {};
+
+app.Router = Backbone.Router.extend({
+	routes: {
+		
+	},
+
+	initialize: function() {
+		
+	}
+});
+
+app.router = new app.Router();
+Backbone.history.start();
